545dc123b16520a748d31048fd536957c86135cc
yourUserName <mfedorukk@gmail.com>
2022-01-24 20:59:43 +0200

    git test reset comand

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 let btn = document.getElementById('button')
+let nav = document.getElementById('nav')
+let header = document.getElementById('header')
 btn.onclick = function() {
     btn.classList.toggle('change')
+    nav.classList.toggle('active')
+    header.classList.toggle('clicked')
 }
