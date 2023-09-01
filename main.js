fdd8823d6616e4046f5a9357a1dd9124443a98c3
yourUserName <mfedorukk@gmail.com>
2023-09-01 10:31:05 +0300

    last update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let burger = document.getElementById('burger')
 let header = document.getElementById('header')
+let header_container = document.getElementById('header-container')
 
 function isClicked() {
     header.classList.toggle('active')
@@ -11,3 +12,16 @@ function isClicked() {
 }
 
 burger.addEventListener('click', isClicked, false)
+window.addEventListener('scroll', trackScroll);
+
+function trackScroll() {
+    let scroll = window.pageYOffset;
+    if (scroll > 100) {
+        header.style.position = 'sticky'
+        header.style.boxShadow = '8px 10px 20px black'
+        header.style.backgroundColor = 'rgb(255, 255, 255, 1)'
+        header.style.opacity = '1'
+    } else if (scroll < 100) {
+        header.style.boxShadow = 'none'
+    }
+}
